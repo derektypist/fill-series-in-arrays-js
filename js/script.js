744cef8d8a9e9f2d7c6f93ae3272c5ec7d40a34a
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-02-01 16:44:13 +0000

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ let txt = "";
 // Set Up Arrays
 let natural = (s,e) => Array(e+1-s).fill(s).map((e,i) => i+1);
 let squares = (s,e) => Array(e+1-s).fill(s).map((e,i) => i**2);
-let cubes =  (s,e) =>Array(e+1-s).fill(s).map((e,i) => i**3);
+let cubes =  (s,e) => Array(e+1-s).fill(s).map((e,i) => i**3);
 let random = Array(10).fill(0).map((i) => Math.floor(Math.random()*100)+1);
-let multOf3and5 = Array(16).fill(false).map((i) => i%3==0 || i%5 == 0 ? true : false);
+let multOf3and5 = (s,e) => Array(e+1-s).fill(false).map((i) => i%3==0 || i%5 == 0 ? true : false);
 
 // Build the text
 txt += `Natural Numbers: ${natural(1,10)} <br>`;
-txt += `Square Numbers: ${squares(1,10)} <br>`;
-txt += `Cube Numbers: ${cubes(1,10)} <br>`;
+txt += `Square Numbers: ${squares(2,11)} <br>`;
+txt += `Cube Numbers: ${cubes(2,11)} <br>`;
 txt += `Random Numbers: ${random} <br>`;
-txt += `Multiples of 3 and 5: ${multOf3and5} <br>`;
+txt += `Multiples of 3 and 5: ${multOf3and5(1,16)} <br>`;
 
 // Display Text in the Browser
 document.getElementById('demo').innerHTML = txt;
