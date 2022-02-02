2b0e998d215ea276afbf640d5810713d6678bc77
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-02-02 10:11:59 +0000

    Added geometric, multOf3Or5 at script.js

--- js/script.js
+++ js/script.js
@@ -6,14 +6,18 @@ let natural = (s,e) => Array(e+1-s).fill(s).map((e,i) => i+1);
 let squares = (s,e) => Array(e+1-s).fill(s).map((e,i) => (i+1)**2);
 let cubes =  (s,e) => Array(e+1-s).fill(s).map((e,i) => (i+1)**3);
 let random = Array(10).fill(0).map((i) => Math.floor(Math.random()*100)+1);
-let multOf3and5 = (s,e) => Array(e+1-s).fill(s).map((e,i) => i> 0 && (i%3==0 || i%5 == 0) ? true : false);
+let geometric = (e) => Array(e).fill(0).map((i) => Math.pow(2,i));
+let isMultOf3Or5 = (e) => Array(e+1).fill(0).map((i) => i%3==0 || i%5 == 0 ? true : false);
+let multOf3Or5 = (e) => Array(e+1).fill(0).map((i) => i%3==0 || i%5 == 0 ? i : 0);
 
 // Build the text
 txt += `Natural Numbers: ${natural(1,10)} <br>`;
 txt += `Square Numbers: ${squares(1,10)} <br>`;
 txt += `Cube Numbers: ${cubes(1,10)} <br>`;
 txt += `Random Numbers: ${random} <br>`;
-txt += `Multiples of 3 and 5: ${multOf3and5(0,15)} <br>`;
+txt += `Geometric Progression: ${geometric(10)} <br>`;
+txt += `Are Multiples of 3 or 5: ${isMultOf3Or5(15)} <br>`;
+txt += `Multiples of 3 or 5: ${multOf3Or5(15)} <br>`;
 
 // Display Text in the Browser
 document.getElementById('demo').innerHTML = txt;
