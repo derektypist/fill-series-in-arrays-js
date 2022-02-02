714a94e35554fbd056a04ea6d37c60aa7910304e
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-02-02 11:09:34 +0000

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let txt = "";
 // Set Up Arrays
 let natural = (s,e) => Array(e+1-s).fill(s).map((e,i) => i+1);
 let squares = (s,e) => Array(e+1-s).fill(s).map((e,i) => (i+1)**2);
-let cubes =  (s,e) => Array(e+1-s).fill(s).map((e,i) => (i+1)**3);
+let cubes = (s,e) => Array(e+1-s).fill(s).map((e,i) => (i+1)**3);
 let random = Array(10).fill(0).map((i) => Math.floor(Math.random()*100)+1);
 let arithmetic = (e) => Array(e).fill(0).map((i) => i+3);
 let geometric = (e) => Array(e).fill(0).map((i) => Math.pow(2,i));
@@ -16,6 +16,7 @@ txt += `Natural Numbers: ${natural(1,10)} <br>`;
 txt += `Square Numbers: ${squares(1,10)} <br>`;
 txt += `Cube Numbers: ${cubes(1,10)} <br>`;
 txt += `Random Numbers: ${random} <br>`;
+txt += `Arithmetic Progression: ${arithmetic(10)} <br>`;
 txt += `Geometric Progression: ${geometric(10)} <br>`;
 txt += `Are Multiples of 3 or 5: ${isMultOf3Or5(15)} <br>`;
 txt += `Multiples of 3 or 5: ${multOf3Or5(15)} <br>`;
