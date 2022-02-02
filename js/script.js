8c05583b380c2d85fee29c73c0ed1a601a09175b
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-02-02 16:51:31 +0000

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ let cubes = (s,e) => Array(e+1-s).fill(s).map((e,i) => (i+1)**3);
 let random = Array(10).fill(0).map((i) => Math.floor(Math.random()*100)+1);
 let arithmetic = (s,e) => Array(e+1-s).fill(s).map((e,i) => i+3);
 let geometric = (s,e) => Array(e+1-s).fill(s).map((e,i) => Math.pow(2,i));
-let isMultOf3Or5 = (s,e) => Array(e+1-s).fill(s).map((e,i) => i%3==0 || i%5 == 0 ? true : false);
-let multOf3Or5 = (s,e) => Array(e+1-s).fill(s).map((e,i) => i%3==0 || i%5 == 0 ? i : 0);
+let isMultOf3Or5 = (s,e) => Array(e+1-s).fill(s).map((e,i) => i+1%3==0 || i+1%5 == 0 ? true : false);
+let multOf3Or5 = (s,e) => Array(e+1-s).fill(s).map((e,i) => i+1%3==0 || i+1%5 == 0 ? i : 0);
 
 // Build the text
 txt += `Natural Numbers: ${natural(1,10)} <br>`;
@@ -18,8 +18,8 @@ txt += `Cube Numbers: ${cubes(1,10)} <br>`;
 txt += `Random Numbers: ${random} <br>`;
 txt += `Arithmetic Progression: ${arithmetic(1,10)} <br>`;
 txt += `Geometric Progression: ${geometric(1,10)} <br>`;
-txt += `Are Multiples of 3 or 5: ${isMultOf3Or5(0,16)} <br>`;
-txt += `Multiples of 3 or 5: ${multOf3Or5(0,16)} <br>`;
+txt += `Are Multiples of 3 or 5: ${isMultOf3Or5(0,10)} <br>`;
+txt += `Multiples of 3 or 5: ${multOf3Or5(0,10)} <br>`;
 
 // Display Text in the Browser
 document.getElementById('demo').innerHTML = txt;
